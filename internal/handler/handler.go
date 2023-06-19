6b9ef89d9f4cfed0c59326217660d06891750a73
Danila Egorov <c4nsero@mail.ru>
2023-06-19 11:52:34 +0300

    update handler: update Respond()
    Добавил проверку на стикер
    Обновил механику проверки реквеста

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,28 +1,46 @@
 package handler
 
 import (
+	"strings"
 	"tgBot/internal/commands"
 	"tgBot/internal/send"
 	"tgBot/internal/telegram"
 )
 
 // Обработчик сообщений
-func Respond(botUrl string, update telegram.Update) error {
+func Respond(botUrl string, update telegram.Update) {
 
-	// msg - текст полученного сообщения
-	msg := update.Message.Text
-	id := update.Message.Chat.ChatId
+	// Запись айди чата
+	chatId := update.Message.Chat.ChatId
 
-	// Обработчик комманд
-	switch msg {
-	case "/hello":
-		send.SendMsg(botUrl, id, "101!")
-		return nil
-	case "/help":
-		commands.Help(botUrl, id)
-		return nil
+	// Обработчик команд
+	if update.Message.Text != "" {
+
+		request := strings.Split(update.Message.Text, " ")
+
+		// Вывод реквеста для тестов
+		// fmt.Println("request: \t", request)
+
+		switch request[0] {
+		case "/start", "/help":
+			commands.Help(botUrl, chatId)
+		case "/hello":
+			send.SendMsg(botUrl, chatId, "101!")
+		default:
+			// Дефолтный ответ
+			send.SendMsg(botUrl, chatId, "У меня пока нет такой команды")
+		}
+
+	} else {
+
+		// Проверка на стикер
+		if update.Message.Sticker.File_id != "" {
+			send.SendStck(botUrl, chatId, "CAACAgIAAxkBAAIaImHkPqF8-PQVOwh_Kv1qQxIFpPyfAAJXAAOtZbwUZ0fPMqXZ_GcjBA")
+		} else {
+			// Если пользователь отправил не сообщение и не стикер:
+			send.SendMsg(botUrl, chatId, "Пока я воспринимаю только текст и стикеры")
+			send.SendStck(botUrl, chatId, "CAACAgIAAxkBAAIaImHkPqF8-PQVOwh_Kv1qQxIFpPyfAAJXAAOtZbwUZ0fPMqXZ_GcjBA")
+		}
+
 	}
-
-	send.SendMsg(botUrl, id, "Я не понимаю, чтобы узнать список команд, воспользуйтесь /help")
-	return nil
 }
