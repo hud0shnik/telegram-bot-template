094f1257f4e46574d64fd7ee182ac18d30942352
Danila Egorov <c4nsero@mail.ru>
2023-08-05 10:10:27 +0300

    update internal: update telegram
    Перенёс send в telegram

--- internal/commands/commands.go
+++ internal/commands/commands.go
@@ -1,12 +1,10 @@
 package commands
 
-import (
-	"tgBot/internal/send"
-)
+import "tgBot/internal/telegram"
 
 // Вывод списка всех команд
 func Help(botUrl string, ChatId int) {
-	send.SendMsg(botUrl, ChatId, "Привет👋🏻, вот список команд:\n\n"+
+	telegram.SendMsg(botUrl, ChatId, "Привет👋🏻, вот список команд:\n\n"+
 		"/help - список команд\n"+
 		"/hello - команда 1\n")
 }
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -3,7 +3,6 @@ package handler
 import (
 	"strings"
 	"tgBot/internal/commands"
-	"tgBot/internal/send"
 	"tgBot/internal/telegram"
 )
 
@@ -25,21 +24,21 @@ func Respond(botUrl string, update telegram.Update) {
 		case "/start", "/help":
 			commands.Help(botUrl, chatId)
 		case "/hello":
-			send.SendMsg(botUrl, chatId, "101!")
+			telegram.SendMsg(botUrl, chatId, "101!")
 		default:
 			// Дефолтный ответ
-			send.SendMsg(botUrl, chatId, "У меня пока нет такой команды")
+			telegram.SendMsg(botUrl, chatId, "У меня пока нет такой команды")
 		}
 
 	} else {
 
 		// Проверка на стикер
 		if update.Message.Sticker.File_id != "" {
-			send.SendStck(botUrl, chatId, "CAACAgIAAxkBAAIaImHkPqF8-PQVOwh_Kv1qQxIFpPyfAAJXAAOtZbwUZ0fPMqXZ_GcjBA")
+			telegram.SendStck(botUrl, chatId, "CAACAgIAAxkBAAIaImHkPqF8-PQVOwh_Kv1qQxIFpPyfAAJXAAOtZbwUZ0fPMqXZ_GcjBA")
 		} else {
 			// Если пользователь отправил не сообщение и не стикер:
-			send.SendMsg(botUrl, chatId, "Пока я воспринимаю только текст и стикеры")
-			send.SendStck(botUrl, chatId, "CAACAgIAAxkBAAIaImHkPqF8-PQVOwh_Kv1qQxIFpPyfAAJXAAOtZbwUZ0fPMqXZ_GcjBA")
+			telegram.SendMsg(botUrl, chatId, "Пока я воспринимаю только текст и стикеры")
+			telegram.SendStck(botUrl, chatId, "CAACAgIAAxkBAAIaImHkPqF8-PQVOwh_Kv1qQxIFpPyfAAJXAAOtZbwUZ0fPMqXZ_GcjBA")
 		}
 
 	}
